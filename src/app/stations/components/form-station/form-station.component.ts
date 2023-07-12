1019baa19a3a86b71e76030c6c74ce04d7be18a6
Kevin Morales <keviinmoralees132@gmail.com>
2023-07-12 15:18:53 -0500

    se dejan validaciones simples listas

--- src/app/stations/components/form-station/form-station.component.ts
+++ src/app/stations/components/form-station/form-station.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FormValidatorsService } from 'src/app/shared/services/form-validators.service';
 
 @Component({
   selector: 'form-station',
@@ -12,7 +13,7 @@ export class FormStationComponent implements OnInit {
 
     @Input()
     public formStation: FormGroup = this.formBuilder.group({
-       name:['', [Validators.required]],
+       name:['', [Validators.required,Validators.minLength(5)]],
        latitud: [0, [Validators.required]],
        longitud: [0, [Validators.required]],
        observacion: ['', [Validators.required]],
@@ -22,12 +23,31 @@ export class FormStationComponent implements OnInit {
 
     @Input()
     public method!: string;
+    public isDirty: boolean = false;
 
-    constructor(private formBuilder: FormBuilder, private router: Router){}
+    constructor(private formBuilder: FormBuilder, private router: Router, private formValidatorService:FormValidatorsService){}
+
+    isValidField( field : string){
+        return this.formValidatorService.isValidField(this.formStation, field)
+    }
+
+    getFieldError(field: string): string | null{
+        return this.formValidatorService.getFieldError(this.formStation,field)
+    }
+
 
     ngOnInit(): void {}
 
     onSend():void{
+
+        if(this.formStation.invalid){
+            console.log('invalido');
+            console.log(this.formStation.controls['latitud'].errors);
+            this.isDirty = true;
+            this.formStation.markAllAsTouched();
+            return
+        }
+
         if(this.method == 'create'){
             console.log(`CREAR ${this.formStation.value}`);
             return;
